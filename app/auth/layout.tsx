ed9eedb4a08a3e159ffe1d8924b14ad8162bd5f1
Florin Große <florin.grosse@idot-digital.de>
2025-10-20 20:35:24 +0200

    Changed tab title for auth routes

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,7 +1,7 @@
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Login",
+  title: "R6 Strats Login",
 };
 
 export default function Layout({ children }: { children: React.ReactNode }) {
